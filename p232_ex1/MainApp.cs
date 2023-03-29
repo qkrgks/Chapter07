9befd7197224f95978c00ec6ef025e5fb7558fb5
한 박 <djakrma0053@naver.com>
2023-03-29 15:45:03 +0900

    202303291544

---
+++ p232_ex1/MainApp.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p232_ex1
+{
+    class Global
+{
+
+    public static int Count = 0;
+
+}
+
+    class ClassA
+    {
+
+        public ClassA()
+        {
+
+            Global.Count++;
+        
+        }
+    
+    
+    }
+
+    class ClassB
+    {
+
+        public ClassB()
+        {
+
+            Global.Count++;
+
+        }
+
+
+    }
+
+
+    class MainApp
+    {
+        static void Main()
+        {
+
+            Console.WriteLine($"Global.Count : {Global.Count}");
+
+            new ClassA();
+            new ClassA();
+            new ClassB();
+            new ClassB();
+
+            Console.WriteLine($"Global.Count : {Global.Count}");
+
+        }
+    }
+}
---
+++ p240_ex1/MainApp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p240_ex1
+{
+
+    class Employee
+    {
+        private string Name;
+        private string Position;
+
+        public void SetName(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetPosition(string Position)
+        {
+            this.Position = Position;
+        }
+
+        public string GetPosition()
+        {
+            return this.Position;
+        }
+
+
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Employee pooh = new Employee();
+            pooh.SetName("Piih");
+            pooh.SetPosition("Waiter");
+            Console.WriteLine($"{pooh.GetName()} {pooh.GetPosition()}");
+
+            Employee tigger = new Employee();
+            pooh.SetName("Tigger");
+            pooh.SetPosition("Cleaner");
+            Console.WriteLine($"{pooh.GetName()} {pooh.GetPosition()}");
+
+        }
+    }
+}
